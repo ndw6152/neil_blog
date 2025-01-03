a324f11664f1dd8568f21a9e8e1d23fb837c7806
Neil <ndwong19@gmail.com>
2025-01-03 13:55:16 -0800

    initial commit react, typescript, webpack, prettier

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return <div>Hello, React with TypeScript and Webpack!</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+  },
+  mode: 'development',
+};
