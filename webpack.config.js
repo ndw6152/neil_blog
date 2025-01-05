7eb386ab09a5ab3bb0fa132bcf7d4a38353c59a2
Neil <ndwong19@gmail.com>
2025-01-04 23:16:23 -0800

    Added css minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const buildMode = process.env.NODE_ENV || 'development'; // Default to 'development' if NODE_ENV is not set
 
 module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true,
+    filename: '[name].[contenthash].js', // Use content hash for cache-busting
+    clean: true, // Clean the output directory before each build
   },
   module: {
     rules: [
@@ -18,7 +22,7 @@ module.exports = {
       {
         test: /\.module\.scss$/, // Handle SCSS modules (files ending with .module.scss)
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader, // Extract CSS into separate files
           {
             loader: 'css-loader',
             options: {
@@ -35,7 +39,7 @@ module.exports = {
         test: /\.scss$/, // Handle global SCSS files (not modules)
         exclude: /\.module\.scss$/, // Exclude .module.scss files
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader, // Extract CSS into separate files
           'css-loader', // No modules option here
           'sass-loader',
         ],
@@ -53,11 +57,27 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css', // Output CSS file name with content hash
+    }),
   ],
+  optimization: {
+    minimize: true, // Minimize JavaScript
+    minimizer: [
+      new TerserPlugin({
+        parallel: true, // Use multiple cores for faster minification
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all', // Split dependencies into separate chunks
+      maxInitialRequests: 5, // Control number of initial requests
+      minSize: 0, // Prevent the creation of small bundles
+    },
+  },
   devServer: {
     static: './dist',
     port: 3000,
   },
-  devtool: 'source-map',
-  mode: 'development',
+  devtool: buildMode === 'production' ? 'source-map' : 'eval-source-map', // Use different source maps depending on mode
+  mode: buildMode,
 };
